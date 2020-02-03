b093d3468f21692f48202574ffea871d229e68d3
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-02-03 16:30:27 +0200

    Exported `rastro_degr`

--- R/degr.R
+++ R/degr.R
@@ -1,14 +1,30 @@
 # CTOR
+#' @title Degree
+#' @param deg Degrees to store.
+#' @param x,y \code{vec_ptype2*} arguments.
+#' @param to \code{vec_cast} argument.
+#' @param x_arg,y_arg \code{vec_ptype2} error message variable names.
+#' @param op,.fn Arithmetic functions/operators.
+#' @param .x \code{vec_arith*} argument.
+#' @param format,na_string \code{glue} flromat strings (support interpolation).
+#' @param ... Additional parameters.
+#' @rdname rastro_degr
+#' @export
 new_degr <- function(deg = double()) {
     new_vctr(vec_cast(deg, double(), x_arg = "deg") %% 360, class = "rastro_degr")
 }
 
+
+#' @rdname rastro_degr
+#' @export
 na_degr <- function() new_degr(NA_real_)
 
 # FORMAT
+#' @rdname rastro_degr
+#' @export
 format.rastro_degr <- function(
     x,
-    format = "{deg}°",
+    format = "{deg}\u00B0",
     na_string = "NA_rastro_degr_",
     ...) {
     deg <- vec_data(x)
@@ -19,60 +35,147 @@ format.rastro_degr <- function(
 }
 
 # METADATA
+#' @rdname rastro_degr
+#' @export
 vec_ptype_abbr.rastro_degr <- function(x, ...) "degr"
+#' @rdname rastro_degr
+#' @export
 vec_ptype_full.rastro_degr <- function(x, ...) "rastro_degr"
 
 # PTYPE
+#' @rdname rastro_degr
+#' @export
 vec_ptype2.rastro_degr <- function(x, y, ...) UseMethod("vec_ptype2.rastro_degr", y)
+
+#' @rdname rastro_degr
+#' @method vec_ptype2.rastro_degr default
+#' @export
 vec_ptype2.rastro_degr.default <- function(x, y, ..., x_arg = "x", y_arg = "y")
-        vec_default_ptype2(x, y, x_arg = x_arg, y_arg = y_arg)
+    vec_default_ptype2(x, y, x_arg = x_arg, y_arg = y_arg)
+
+#' @rdname rastro_degr
+#' @method vec_ptype2.rastro_degr rastro_degr
+#' @export
 vec_ptype2.rastro_degr.rastro_degr <- function(x, y, ...) new_degr()
+#' @rdname rastro_degr
+#' @method vec_ptype2.rastro_degr double
+#' @export
 vec_ptype2.rastro_degr.double <- function(x, y, ...) new_degr()
+#' @rdname rastro_degr
+#' @method vec_ptype2.rastro_degr integer
+#' @export
 vec_ptype2.rastro_degr.integer <- function(x, y, ...) new_degr()
+#' @rdname rastro_degr
+#' @method vec_ptype2.integer rastro_degr
+#' @export
 vec_ptype2.integer.rastro_degr <- function(x, y, ...) new_degr()
+#' @rdname rastro_degr
+#' @method vec_ptype2.double rastro_degr
+#' @export
 vec_ptype2.double.rastro_degr <- function(x, y, ...) new_degr()
 
+#' @rdname rastro_degr
+#' @method vec_ptype2.rastro_dec rastro_degr
+#' @export
 vec_ptype2.rastro_dec.rastro_degr <- function(x, y, ...) new_degr()
+#' @rdname rastro_degr
+#' @method vec_ptype2.rastro_ra rastro_degr
+#' @export
 vec_ptype2.rastro_ra.rastro_degr <- function(x, y, ...) new_degr()
 
+#' @rdname rastro_degr
+#' @method vec_ptype2.rastro_degr rastro_dec
+#' @export
 vec_ptype2.rastro_degr.rastro_dec <- function(x, y, ...) new_degr()
+#' @rdname rastro_degr
+#' @method vec_ptype2.rastro_degr rastro_ra
+#' @export
 vec_ptype2.rastro_degr.rastro_ra <- function(x, y, ...) new_degr()
 
 
-# LINKING PTYPE
+ #LINKING PTYPE
+#' @rdname rastro_degr
+#' @method vec_ptype2.rastro_dec rastro_ra
+#' @export
 vec_ptype2.rastro_dec.rastro_ra <- function(x, y, ...) new_degr()
+#' @rdname rastro_degr
+#' @method vec_ptype2.rastro_ra rastro_dec
+#' @export
 vec_ptype2.rastro_ra.rastro_dec <- function(x, y, ...) new_degr()
 
-
+#' @rdname rastro_degr
+#' @export
 is_degr <- function(x) vec_is(x, new_degr())
 
 
 # CAST
+#' @rdname rastro_degr
+#' @export
 vec_cast.rastro_degr <- function(x, to, ...) UseMethod("vec_cast.rastro_degr")
+#' @rdname rastro_degr
+#' @method vec_cast.rastro_degr default
+#' @export
 vec_cast.rastro_degr.default <- function(x, to, ...) vec_default_cast(x, to)
+#' @rdname rastro_degr
+#' @method vec_cast.rastro_degr rastro_degr
+#' @export
 vec_cast.rastro_degr.rastro_degr <- function(x, to, ...) x
-vec_cast.rastro_degr.integer <- function(x, to, ...) new_degr(x)
+#' @rdname rastro_degr
+#' @method vec_cast.rastro_degr double
+#' @export
 vec_cast.rastro_degr.double <- function(x, to, ...) new_degr(x)
+#' @rdname rastro_degr
+#' @method vec_cast.rastro_degr integer
+#' @export
+vec_cast.rastro_degr.integer <- function(x, to, ...) new_degr(x)
+#' @rdname rastro_degr
+#' @method vec_cast.rastro_degr rastro_dec
+#' @export
 vec_cast.rastro_degr.rastro_dec <- function(x, to, ...) new_degr(dec_2_deg(x))
+#' @rdname rastro_degr
+#' @method vec_cast.rastro_degr rastro_ra
+#' @export
 vec_cast.rastro_degr.rastro_ra <- function(x, to, ...) new_degr(ra_2_hr(x) * 15)
 
+#' @rdname rastro_degr
+#' @method vec_cast.double rastro_degr
+#' @export
 vec_cast.double.rastro_degr <- function(x, to, ...) vec_data(x)
+#' @rdname rastro_degr
+#' @method vec_cast.integer rastro_degr
+#' @export
 vec_cast.integer.rastro_degr <- function(x, to, ...) vec_cast(vec_data(x), integer())
-
+#' @rdname rastro_degr
+#' @method vec_cast.rastro_dec rastro_degr
+#' @export
 vec_cast.rastro_dec.rastro_degr <- function(x, to, ...) new_dec_from_degr(vec_data(x))
+#' @rdname rastro_degr
+#' @method vec_cast.rastro_ra rastro_degr
+#' @export
 vec_cast.rastro_ra.rastro_degr <- function(x, to, ...) new_ra_from_hr(vec_data(x) / 15)
 
+#' @rdname rastro_degr
+#' @export
 as_degr <- function(x) vec_cast(x, new_degr())
 
 # EQUALITY
+#' @rdname rastro_degr
+#' @export
 `%==%.rastro_degr` <- function(x, y) UseMethod("%==%.rastro_degr", y)
-`%==%.rastro_degr.default` <- function(x, y) vec_equal(x, y)
-`%==%.rastro_degr.rastro_degr` <- function(x, y)
-    vec_data(x) %==% vec_data(y)
+#' @rdname rastro_degr
+#' @method %==%.rastro_degr default
+#' @export
+`%==%.rastro_degr.default` <- function(x, y) vec_equal(x, y) %|% FALSE
 
 # ARITHMETIC
 vec_arith.rastro_degr <- function(op, x, y, ...) UseMethod("vec_arith.rastro_degr", y)
+#' @rdname rastro_degr
+#' @method vec_arith.rastro_degr default
+#' @export
 vec_arith.rastro_degr.default <- function(op, x, y, ...) stop_incompatible_op(op, x, y)
+#' @rdname rastro_degr
+#' @method vec_arith.rastro_degr MISSING
+#' @export
 vec_arith.rastro_degr.MISSING <- function(op, x, y, ...) {
     if (op %==% "-") {
         return(new_degr(-vec_data(x)))
@@ -80,6 +183,9 @@ vec_arith.rastro_degr.MISSING <- function(op, x, y, ...) {
         return(x)
     stop_incompatible_op(op, x, y)
 }
+#' @rdname rastro_degr
+#' @method vec_arith.rastro_degr numeric
+#' @export
 vec_arith.rastro_degr.numeric <- function(op, x, y, ...) {
     vec_recycle_common(x, y) %->% c(x, y)
     data_x <- vec_data(x)
@@ -91,6 +197,9 @@ vec_arith.rastro_degr.numeric <- function(op, x, y, ...) {
         "/" = new_degr(data_x / y),
         stop_incompatible_op(op, x, y))
 }
+#' @rdname rastro_degr
+#' @method vec_arith.numeric rastro_degr
+#' @export
 vec_arith.numeric.rastro_degr <- function(op, x, y, ...) {
     vec_recycle_common(x, y) %->% c(x, y)
     data_y <- vec_data(y)
@@ -101,6 +210,9 @@ vec_arith.numeric.rastro_degr <- function(op, x, y, ...) {
         "*" = new_degr(x * data_y),
         stop_incompatible_op(op, x, y))
 }
+#' @rdname rastro_degr
+#' @method vec_arith.rastro_degr rastro_degr
+#' @export
 vec_arith.rastro_degr.rastro_degr <- function(op, x, y, ...) {
     vec_recycle_common(x, y) %->% c(x, y)
     data_x <- vec_data(x)
@@ -113,11 +225,8 @@ vec_arith.rastro_degr.rastro_degr <- function(op, x, y, ...) {
         stop_incompatible_op(op, x, y))
 }
 
-#vec_arith.rastro_degr.integer <- function(op, x, y, ...)
-    #vec_arith.rastro_degr.double(op, x, vec_cast(y, double()), ...)
-#vec_arith.integer.rastro_degr <- function(op, x, y, ...)
-    #vec_arith.double.rastro_degr(op, vec_cast(x, double()), y, ...)
-
+#' @rdname rastro_degr
+#' @export
 vec_math.rastro_degr <- function(.fn, .x, ...) {
     data_x <- vec_data(.x)
     switch(.fn,
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,17 @@
-#' @importFrom rlang        eval_tidy parse_expr is_na is_empty
-#' @importFrom vctrs        vec_cast vec_recycle_common new_rcrd field
-#' @importFrom vctrs        stop_incmpatible_type stop_incompatible_op stop_incompatible_cast
-#' @importFrom primitiveR   %->% glue_fmt_chr assert vmap_if cc
+#' @importFrom rlang        eval_tidy parse_expr is_na is_empty %|% %@% %||% missing_arg
+#' @importFrom rlang        abort is_missing
+#'
+#' @importFrom vctrs        vec_cast vec_recycle_common new_rcrd field %0% vec_equal vec_c
+#' @importFrom vctrs        stop_incompatible_type stop_incompatible_op stop_incompatible_cast
+#' @importFrom vctrs        vec_arith.numeric vec_cast.double vec_cast.integer vec_slice
+#' @importFrom vctrs        vec_ptype2.double vec_ptype2.integer vec_data vec_assert vec_repeat
+#' @importFrom vctrs        vec_size vec_is vec_init vec_is_empty new_vctr vec_ptype
+#' @importFrom vctrs        vec_proxy_equal vec_arith vec_ptype_common allow_lossy_cast
+#' @importFrom vctrs        vec_cast_common vec_arith_base vec_ptype2 maybe_lossy_cast
+#' @importFrom vctrs        vec_as_location vec_cbind vec_default_cast vec_default_ptype2
+#' @importFrom vctrs        vec_math_base vec_proxy vec_proxy_compare vec_rbind
+#' @importFrom vctrs        vec_restore vec_seq_along vec_slice<-
+#'
+#' @importFrom primitiveR   %->% glue_fmt_chr assert vmap_if cc %==% %===% %!=% %!==% are_equal_f
+#' @importFrom primitiveR   vmap_pt vmap vmap2 %&% len vec_item_ptype
 NULL
